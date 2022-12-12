fa545565ef45d30ddcf74d3fb5185c8eead51cf5
Dimon <d.dunaiskyi@everstake.one>
2022-12-12 22:35:42 +0100

    Update entrypoint.sh and Caddyfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,60 @@
 #!/bin/sh
 
-set -e
 
-ENDPOINT_UPDATE_INTERVAL=3600
-ENDPOINT_COUNT=30
-ENDPOINT_FORMAT="haproxy"
-ENDPOINT_LIST=$( wget -qO- 'https://api.extrnode.com/endpoints?limit='${ENDPOINT_COUNT}'&format='${ENDPOINT_FORMAT}'&node_type=full_rpc' | awk '{printf "%s" " " ,$0}' ) caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+RD='\e[1;31m'
+YL='\e[1;33m'
+GR='\e[1;32m'
+BL='\e[1;34m'
+NC='\e[1;0m'
 
-echo "Endpoint list: ${ENDPOINT_LIST}"
+UPSTREAMS_UPDATE_INTERVAL=3600
+UPSTREAMS_NUMBER=30
+LOWER_UPSTREAMS_NUMBER_LIMIT=3
+UPSTREAMS_RESPONSE_FORMAT="haproxy"
+UPSTREAMS_API_URL="https://api.extrnode.com/endpoints?limit=${UPSTREAMS_NUMBER}&format=${UPSTREAMS_RESPONSE_FORMAT}&node_type=full_rpc"
+FULL_REQUEST_BODY="wget -qO- ${UPSTREAMS_API_URL}"
 
-while true
+get_upstreams_list () {
+	RAW_REQUEST_BODY=$( ${FULL_REQUEST_BODY} )
+	if [ $? -eq 0 ]; then
+		echo -e "$( date '+%d/%b/%Y:%T %z' )\t${GR}SUCCESS${NC}\tRequest get upstreams list success. Start formating request"
+	else
+		echo -e "$( date '+%d/%b/%Y:%T %z' )\t${RD}FAIL${NC}\tRequest get upstreams list failed. Exit..."
+		exit 1
+	fi
+
+	if [ $( printf '%s' "${RAW_REQUEST_BODY}" | wc -l ) -le ${LOWER_UPSTREAMS_NUMBER_LIMIT} ]; then
+		echo -e "$( date '+%d/%b/%Y:%T %z' )\t${RD}FAIL${NC}\tRequest get upstreams list back with number upstream < ${LOWER_UPSTREAMS_NUMBER_LIMIT}. Exit..."
+		exit 1
+	fi
+
+	FORMAT_RESPONSE_BODY="$( printf '%s' "${RAW_REQUEST_BODY}" | awk '{printf "%s" " " ,$0}' )"
+	if [ $? -eq 0 ]; then
+		echo -e "$( date '+%d/%b/%Y:%T %z' )\t${GR}SUCCESS${NC}\tFormating list success. Start caddy reverse proxy with formated list"
+	else
+		echo -e "$( date '+%d/%b/%Y:%T %z' )\t${RD}FAIL${NC}\tFormating list failed. Exit..."
+		exit 1
+	fi
+
+	UPSTREAMS_LIST="${FORMAT_RESPONSE_BODY}" $@
+	if [ $? -eq 0 ]; then
+		echo -e "$( date '+%d/%b/%Y:%T %z' )\t${GR}SUCCESS${NC}\tUsed upstreams list: ${FORMAT_RESPONSE_BODY}"
+	else
+		echo -e "$( date '+%d/%b/%Y:%T %z' )\t${RD}FAIL${NC}\tStart caddy reverse proxy failed. Exit..."
+		exit 1
+	fi
+}
+
+i=0
+while [ "$i" -ge 0 ]
 do
-	sleep "${ENDPOINT_UPDATE_INTERVAL}"
-	ENDPOINT_LIST=$( wget -qO- 'https://api.extrnode.com/endpoints?limit='${ENDPOINT_COUNT}'&format='${ENDPOINT_FORMAT}'&node_type=full_rpc' | awk '{printf "%s" " " ,$0}' ) caddy reload --config /etc/caddy/Caddyfile
+	if [ $i -eq 0 ]; then
+		get_upstreams_list caddy start --config /etc/caddy/Caddyfile --adapter caddyfile
+
+	else
+		get_upstreams_list caddy reload --config /etc/caddy/Caddyfile
+	fi
+	if [ $? -ne 0 ]; then break; fi
+	sleep "${UPSTREAMS_UPDATE_INTERVAL}"
+	i=$(( i + 1 ))
 done
